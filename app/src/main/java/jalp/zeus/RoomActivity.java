d94340e666f6be2630b2027b5a3a876be9ca12c1
Liam <lcottier94@googlemail.com>
2016-02-23 16:36:09 +0000

    Added RoomActivity activity

--- app/src/main/java/jalp/zeus/RoomActivity.java
+++ app/src/main/java/jalp/zeus/RoomActivity.java
@@ -12,7 +12,18 @@ public class RoomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash_board);
+        setContentView(R.layout.activity_room);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 }
