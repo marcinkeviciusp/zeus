141fe0bc215ceac53dc0663a24082d85e3e3ced3
Ansonhkg <ansonox@gmail.com>
2016-02-23 14:13:50 +0000

    Add light page

--- app/src/main/java/jalp/zeus/DataTrendsActivity.java
+++ app/src/main/java/jalp/zeus/DataTrendsActivity.java
@@ -1,13 +1,17 @@
 package jalp.zeus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class DataTrendsActivity extends AppCompatActivity {
+
+    Button mBtnLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,13 @@ public class DataTrendsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        mBtnLight = (Button) findViewById(R.id.btnLight);
+
+        mBtnLight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(DataTrendsActivity.this, GraphLightActivity.class));
             }
-        });*/
+        });
     }
 
 }
