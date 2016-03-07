281e6a4ad37c9dcc58929d58d6f195fe6066c54d
Liam <lcottier94@googlemail.com>
2016-03-07 20:23:35 +0000

    Merged

--- app/src/main/java/jalp/zeus/SpotActivity.java
+++ app/src/main/java/jalp/zeus/SpotActivity.java
@@ -10,6 +10,7 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -231,7 +232,6 @@ public class SpotActivity extends AppCompatActivity {
 
     private void removeType(char character){
         if(character == 'c'){
-            System.out.println("In Compass");
             TextView text = (TextView) findViewById(R.id.compassText);
             text.setVisibility(View.GONE);
         }else if(character == 't'){
@@ -261,17 +261,6 @@ public class SpotActivity extends AppCompatActivity {
             TextView text = (TextView) findViewById(R.id.infraredText);
             text.setVisibility(View.GONE);
         }
-    }
-
-    private boolean typeUsed(char character){
-        String allTypes = "ctlabrseiwxyz";
-        boolean unused = true;
-        for(int i=0;i<allTypes.length();i++){
-            if(character == allTypes.charAt(i)){
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
